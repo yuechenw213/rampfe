21f0a1e2cb90cfb9835106d5e9d49ff111b421d9
Yuechen <92114903+yuechenw213@users.noreply.github.com>
2023-07-19 17:42:58 +0000

    fix bug 5

--- src/App.tsx
+++ src/App.tsx
@@ -24,9 +24,8 @@ export function App() {
     transactionsByEmployeeUtils.invalidateData()
 
     await employeeUtils.fetchAll()
-    await paginatedTransactionsUtils.fetchAll()
-
     setIsLoading(false)
+    await paginatedTransactionsUtils.fetchAll()
   }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
 
   const loadTransactionsByEmployee = useCallback(
